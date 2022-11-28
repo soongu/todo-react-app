5e90322b7400dcc580f99fab1a774d45ea653751
soongu <hsg9984@gmail.com>
2022-11-28 17:37:32 +0900

    #4. Todo 컴포넌트 배열로 렌더링

--- src/App.js
+++ src/App.js
@@ -3,11 +3,29 @@ import './App.css';
 import Todo from './Todo';
 
 function App() {
-  return (
-    <div className="App">
-      <Todo />
-    </div>
-  );
+
+    const items = [
+        {
+            id: 1,
+            title: 'hello world1',
+            done: true
+        },
+        {
+            id: 2,
+            title: 'hello world2',
+            done: false
+        }
+    ];
+
+    const todoItems = items.map(item => {
+        return <Todo key={item.id} item={item} />;
+    });
+
+    return (
+        <div className="App">
+            {todoItems}
+        </div>
+    );
 }
 
 export default App;
