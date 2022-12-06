8dca0efa545d6e4cb95a23c0c144b5b4abb49692
soongu <hsg9984@gmail.com>
2022-12-06 16:42:59 +0900

    #11. 로컬스토리지에 토큰 저장하기

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,6 +1,8 @@
 import {API_BASE_URL} from "../config/app-config";
 import axios from "axios";
 
+const ACCESS_TOKEN = 'ACCESS_TOKEN';
+
 export const call = async(apiUri, method='GET', payload={}) => {
 
     const error403 = () => {
@@ -10,10 +12,22 @@ export const call = async(apiUri, method='GET', payload={}) => {
 
     const url = API_BASE_URL + apiUri;
 
+    // 헤더
+    const headers = {
+        'Content-Type' : 'application/json'
+    };
+
+    // 로컬 스토리지에서 토큰 가져오기
+    const accessToken = localStorage.getItem(ACCESS_TOKEN);
+    if (accessToken && accessToken !== null) {
+        headers['Authorization'] = 'Bearer ' + accessToken;
+    }
+
     let responseData = await axios({
         method: method,
         url: url,
-        data: payload
+        data: payload,
+        headers: headers
     }).catch(error => {
         if (error.response.status === 403) error403();
     });
@@ -27,6 +41,8 @@ export const signin = (userInfo) => {
         .then(res => {
             console.log(res);
             if (res.data.token) {
+                // 로컬 스토리지에 토큰 저장
+                localStorage.setItem(ACCESS_TOKEN, res.data.token);
                 window.location.href='/';
             }
         })
